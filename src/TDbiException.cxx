32e75c4462fbdb2d25ff70b0204fd0d3ed081189
Clark McGrew <clark.mcgrew@stonybrook.edu>
2014-10-15 16:54:29 -0400

    Fix TDbiException so that it doesn't core dump on an exception.
    The code was making an assumption that the returned error message pointer
    would not be NULL.  That might be true in mysql, but there are times in
    SQLite where there isn't a provided error message.

--- src/TDbiException.cxx
+++ src/TDbiException.cxx
@@ -25,30 +25,31 @@ ClassImp(CP::TDbiException)
 
 /// Create an exception using the msg and code provided.
 CP::TDbiException::TDbiException(const char* msg /*= 0*/,
-                                 Int_t code      /* = -1 */):
-    fMessage(msg),
-    fErrorCode(code) {
-
+                                 Int_t code      /* = -1 */) {
+    if (msg) fMessage = std::string(msg);
+    else fMessage.clear();
+    fErrorCode = code;
     DbiTrace("Creating CP::TDbiException" << "  ");
-
 }
 
 //.....................................................................
 /// Create an exception, getting the error message and code from server.
-CP::TDbiException::TDbiException(const TSQLServer& server):
-    fMessage(server.GetErrorMsg()),
-    fErrorCode(server.GetErrorCode()) {
-
-    DbiTrace("Creating CP::TDbiException" << "  ");
+CP::TDbiException::TDbiException(const TSQLServer& server) {
+    if (server.GetErrorMsg()) fMessage = std::string(server.GetErrorMsg());
+    else fMessage.clear();
+    fErrorCode = server.GetErrorCode(); 
+    DbiTrace("Creating CP::TDbiException");
 
 }
 
 //.....................................................................
 /// Create and exception, taking the message and code from statement.
-CP::TDbiException::TDbiException(const TSQLStatement& statement):
-    fMessage(statement.GetErrorMsg()),
-    fErrorCode(statement.GetErrorCode()) {
-
+CP::TDbiException::TDbiException(const TSQLStatement& statement) {
+    if (statement.GetErrorMsg()) {
+        fMessage = std::string(statement.GetErrorMsg());
+    }
+    else fMessage.clear();
+    fErrorCode = statement.GetErrorCode();
     DbiTrace("Creating CP::TDbiException" << "  ");
 
 }
@@ -78,8 +79,9 @@ CP::TDbiException::~TDbiException() {
 /// Append message describing this exception to the std::ostream 'os'.
 std::ostream& CP::operator<<(std::ostream& os, const CP::TDbiException& e) {
 
-    os << "Error " << e.GetErrorCode()
-       << " (" << e.GetMessage() << ")"  << std::endl;
+    os << "Error " << e.GetErrorCode();
+    os << " (" << e.GetMessage() << ")";
+    os <<  std::endl;
     return os;
 
 }
